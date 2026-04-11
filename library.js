caf0c2b26e6d22f01c58956b1e046dc719a57986
henthaii <200550625+henthaii@users.noreply.github.com>
2026-04-11 18:44:40 -0500

    added error catch.
    started new func to add book to array.

--- library.js
+++ library.js
@@ -1,6 +1,9 @@
 const myLibrary = [];
 
 function Book(title,author) {
+     if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
     this.title = title;
     this.author = author;
     this.sayBook = function () {

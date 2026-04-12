febdcbcdc5542044f8d2998e697309bde25080d1
henthaii <200550625+henthaii@users.noreply.github.com>
2026-04-11 19:48:09 -0500

    corrected first function to include pages and read parameters. added function info for addBookToLibrary

--- library.js
+++ library.js
@@ -1,16 +1,19 @@
 const myLibrary = [];
 
-function Book(title,author) {
+function Book(title,author,pages,read) {
      if (!new.target) {
     throw Error("You must use the 'new' operator to call the constructor");
   }
     this.title = title;
     this.author = author;
+    this.pages = pages;
+    this.read = read;
     this.sayBook = function () {
         console.log(this.title);
     };
 }
 
-function addBookToLibrary() {
-
+function addBookToLibrary(title,author,pages,read) {
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
 }

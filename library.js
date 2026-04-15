8611fc527ec89d98a6ccc5f5e23f36f36c123f0d
henthaii <200550625+henthaii@users.noreply.github.com>
2026-04-14 21:57:19 -0500

    added more books to populate array data. added test function for displaying library info.

--- library.js
+++ library.js
@@ -8,9 +8,10 @@ function Book(title,author,pages,read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    const id = self.crypto.randomUUID();
+    this.id = self.crypto.randomUUID();
     this.sayBook = function () {
         console.log(this.title);
+        console.log(this.id);
     };
 }
 
@@ -19,7 +20,15 @@ function addBookToLibrary(title,author,pages,read) {
   myLibrary.push(newBook);
 }
 
-new Book("Modern", "Anna", 197, "no");
+new addBookToLibrary("Modern Friendship", "Anna Goldfarb", 197, "no");
+new addBookToLibrary("The Hobbit","J.R.R. Tolkien",320,"no");
+new addBookToLibrary("Harry Potter","J.K. Rowling",269, "yes");
 
-console.log(Book);
+// console.log(Book);
 console.log(myLibrary);
+
+myLibrary.forEach(item => {
+  let li = document.createElement('li');
+  li.innerText = item;
+  document.getElementById('myList').appendChild(li);
+})
